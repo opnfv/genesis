9c13f3590337816816a09954cb7bd3be0302a383
Dan Smith <daniel.smith@ericsson.com>
2015-05-15 16:22:35 -0400

    Update for SR3 Helium for ODL and refactoring other files.
    Change-Id: I5e616652f1084211bab4f8054dbf700112a89774
    JIRA: 0
    Signed-off-by: Dan Smith <daniel.smith@ericsson.com>

--- fuel/build/f_odl_docker/dockerfile/container_scripts/speak.sh
+++ fuel/build/f_odl_docker/dockerfile/container_scripts/speak.sh
@@ -6,8 +6,9 @@
 # Simple expect script to start up ODL client and load feature set for DLUX and OVSDB
 #
 #  NOTE: THIS WILL BE REPLACED WITH A PROGRAMATIC METHOD SHORTLY
+#  DEPRECATED AFTER ARNO
 
-spawn /opt/odl/distribution-karaf-0.2.2-Helium-SR2/bin/client
+spawn /opt/odl/distribution-karaf-0.2.3-Helium-SR3/bin/client
 expect "root>"
 send "feature:install odl-base-all odl-aaa-authn odl-restconf odl-nsf-all odl-adsal-northbound odl-mdsal-apidocs  odl-ovsdb-openstack odl-ovsdb-northbound odl-dlux-core"
 send "\r\r\r"
