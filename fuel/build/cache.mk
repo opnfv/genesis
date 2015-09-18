3ea15b6f4daaad1a1a2b6525f0c7220fa49b72cf
Jonas Bjurel <jonas.bjurel@ericsson.com>
2015-09-18 11:14:28 +0200

    Fixed cache logic
    Change-Id: I904a4b8ebcab5aedefef287ff8bb5357dae8e3ff
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

--- fuel/build/cache.mk
+++ fuel/build/cache.mk
@@ -54,7 +54,7 @@ $(CACHEFILES):
 
 	@if [ ! -f $(BUILD_BASE)/$@ ]; then\
 	   echo " " > $(BUILD_BASE)/$@;\
- 	   ln -s $(BUILD_BASE)/$@ $(CACHE_DIR)/$@;\
+	   ln -s $(BUILD_BASE)/$@ $(CACHE_DIR)/$@;\
 	   rm -f $(BUILD_BASE)/$@;\
 	else\
 	   ln -s $(BUILD_BASE)/$@ $(CACHE_DIR)/$@;\
@@ -62,12 +62,12 @@ $(CACHEFILES):
 
 .PHONY: validate-cache
 validate-cache: prepare $(CACHEVALIDATE)
-	@if [[ $(shell md5sum $(BUILD_BASE)/config.mk | cut -f1 -d " ") != $(shell cat $(VERSION_FILE) | grep config.mk | awk '{print $$NF}') ]]; then\
+	@if [ "$(shell md5sum $(BUILD_BASE)/config.mk | cut -f1 -d " ")" != "$(shell cat $(VERSION_FILE) | grep config.mk | awk '{print $$NF}')" ]; then\
 	   echo "Cache does not match current config.mk definition, cache must be rebuilt";\
 	   exit 1;\
 	fi;
 
-	@if [[ $(shell md5sum $(BUILD_BASE)/cache.mk | cut -f1 -d " ") != $(shell cat $(VERSION_FILE) | grep cache.mk | awk '{print $$NF}') ]]; then\
+	@if [ "$(shell md5sum $(BUILD_BASE)/cache.mk | cut -f1 -d " ")" != "$(shell cat $(VERSION_FILE) | grep cache.mk | awk '{print $$NF}')" ]; then\
 	   echo "Cache does not match current cache.mk definition, cache must be rebuilt";\
 	   exit 1;\
 	fi;
@@ -80,13 +80,11 @@ validate-cache: prepare $(CACHEVALIDATE)
 	then \
 	   REMOTE_ID=$(shell git ls-remote $(FUEL_MAIN_REPO) $(FUEL_MAIN_TAG) | awk '{print $$(NF-1)}'); \
 	fi; \
-	if [ $$REMOTE_ID != $(shell cat $(VERSION_FILE) | grep fuel | awk '{print $$NF}') ]; \
+	if [[ $$REMOTE_ID != $(shell cat $(VERSION_FILE) | grep fuel | awk '{print $$NF}') ]]; \
 	then \
 	   echo "Cache does not match upstream Fuel, cache must be rebuilt!"; \
 	   exit 1; \
 	fi
-
-	#$(MAKE) -C opendaylight validate-cache
 
 .PHONY: $(CACHEVALIDATE)
 $(CACHEVALIDATE): %.validate:
