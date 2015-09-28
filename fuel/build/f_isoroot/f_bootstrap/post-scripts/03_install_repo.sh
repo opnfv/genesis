66b527d7a94ad5be261aec39fe692a135f77b657
Stefan K. Berg <stefan.k.berg@ericsson.com>
2015-09-28 16:05:46 +0200

    Make 03_install_repo.sh executable and fix typo
    Change-Id: I8d1e52f4e38110e55a3d34c46cb899e32e47b8ad
    Signed-off-by: Stefan K. Berg <stefan.k.berg@ericsson.com>
    (cherry picked from commit 72f7e0f9dcb2cbd241a93637c0c99e98dac5fd39)

--- fuel/build/f_isoroot/f_bootstrap/post-scripts/03_install_repo.sh
+++ fuel/build/f_isoroot/f_bootstrap/post-scripts/03_install_repo.sh
@@ -5,7 +5,7 @@ if [ ! -d /opt/opnfv/nailgun ]; then
   exit 1
 fi
 
-mkdir -p /var/www/mailgun
+mkdir -p /var/www/nailgun
 mv /opt/opnfv/nailgun/* /var/www/nailgun
 if [ $? -ne 0 ]; then
   echo "Error moving repos to their correct location!"
