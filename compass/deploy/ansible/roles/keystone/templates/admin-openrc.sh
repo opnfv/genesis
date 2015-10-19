de3139313cb7e3087e3328d1b37cdc88510ca5db
Weidong Shao <weidong.shao@huawei.com>
2015-10-19 15:04:48 +0000

    Remove Compass from genesis.
    Compass4nfv has its own repo.
    
    Change-Id: I4b1b778682e5b49192ab00f5319594c66640a432
    Signed-off-by: Weidong Shao <weidong.shao@huawei.com>

--- compass/deploy/ansible/roles/keystone/templates/admin-openrc.sh
+++
@@ -1,6 +0,0 @@
-# Verify the Identity Service installation
-export OS_PASSWORD={{ ADMIN_PASS }}
-export OS_TENANT_NAME=admin
-export OS_AUTH_URL=http://{{ HA_VIP }}:35357/v2.0
-export OS_USERNAME=ADMIN
-
